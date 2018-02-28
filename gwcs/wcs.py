cc38c034fdf191ec8b1a2444440b3aa95cdd4f87
Stuart Mumford <stuart@cadair.com>
2018-02-28 18:35:06 +0000

    Add a coordinate_to_quantity method to frames
    This allows smarter conversion of inputs to the inverse transform to quantities
    for passing to model.

--- gwcs/wcs.py
+++ gwcs/wcs.py
@@ -1,6 +1,5 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
 import functools
-import warnings
 
 import numpy as np
 from astropy.modeling.core import Model
@@ -16,7 +15,6 @@ __all__ = ['WCS']
 
 
 class WCS:
-
     """
     Basic WCS class.
 
@@ -259,10 +257,9 @@ class WCS:
             raise ValueError("Type of output unrecognized {0}".format(output))
         return result
 
-
     def invert(self, *args, **kwargs):
         """
-        Invert coordnates.
+        Invert coordinates.
 
         The analytical inverse of the forward transform is used, if available.
         If not an iterative method is used.
@@ -279,8 +276,8 @@ class WCS:
         fill_value : float, optional
             Output value for inputs outside the bounding_box (default is np.nan).
         """
-        if not utils.isnumerical(args[0]):
-            args = utils._get_values(self.unit, *args)
+
+        args = self.output_frame.coordinate_to_quantity(*args)
 
         output = kwargs.pop('output', None)
         if 'with_bounding_box' not in kwargs:
